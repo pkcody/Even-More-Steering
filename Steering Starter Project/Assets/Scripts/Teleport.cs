8e8f340ca81867fbd820766f3c43b8a2aa2409ac
pkcody <77869251+pkcody@users.noreply.github.com>
2023-02-11 20:08:23 -0700

    prod

---
+++ Steering Starter Project/Assets/Scripts/Teleport.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    
+    public void PickRandom()
+    {
+        float xrand = Random.Range(-40f, -5.5f);
+        float zrand = Random.Range(-25f, -13f);
+        print(xrand + ", " + zrand);
+        gameObject.transform.position = new Vector3(xrand, gameObject.transform.position.y, zrand);
+
+        Invoke("PickRandom", 5f);
+
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        Invoke("PickRandom", 5f);
+    }
+}
